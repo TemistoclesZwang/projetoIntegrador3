7669fcbdc04d7efdbb38ece1b44873d298a3529e
Temistocles Zwang <temis2st@gmail.com>
2024-06-23 10:30:27 -0300

    ref: modificada homepage

--- star-frontend/src/pages/HomePage/index.tsx
+++ star-frontend/src/pages/HomePage/index.tsx
@@ -6,11 +6,11 @@ import {
   Flex,
   Text,
   useTheme,
-  SimpleGrid,
+  Link,
 } from "@chakra-ui/react";
-import { NavBar } from "../../components/NavBar";
+import { IoSparklesSharp } from "react-icons/io5";
+// import { NavBar } from "../../components/NavBar";
 import parking from "../../assets/HomePage/parking.jpg";
-import { GoogleMapsCard } from "../../components/GoogleMapsCard";
 import { SimpleCard } from "../../components/SimpleCard";
 
 export function HomePage() {
@@ -22,54 +22,72 @@ export function HomePage() {
 
   return (
     <>
+      <style>{`
+        body {
+          overflow: hidden;
+          margin: 0;
+          padding: 0;
+          height: 100vh;
+          width: 100vw;
+          display: flex;
+          flex-direction: column;
+        }
+      `}</style>
       {/* <NavBar /> */}
-      <Flex justify="center" mt={40}>
-        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={1} width="100%">
-          <Card className="mainCard" m={5}>
-            <Box position="relative" mt={10} m={2}>
-              <Image
-                src={parking}
-                alt="Parking"
-                borderRadius={"xl"}
-                w={"100%"}
-                h={"80%"}
-              />
-              <Text
-                textStyle={"titleSize"}
-                fontSize={theme.textStyles?.titleSize}
-                position="absolute"
-                top="-1%"
-                left="50%"
-                transform="translate(-50%, -50%)"
-                color="black"
-                w={"100%"}
-              >
-                Gerencie, lucre, avance
-              </Text>
-              <Box
+      <Flex bgColor={'black'} p={'1rem'}>
+        <Link
+          href="/home"
+          display="flex"
+          alignItems="center"
+          _hover={{ textDecoration: "none" }}
+        >
+          <IoSparklesSharp color={"#98FB98"} fontSize={40} />
+          <Text
+            textStyle={"linkSize"}
+            fontSize={theme.textStyles.linkSize}
+            ml={2}
+            color={'white'}
+          >
+            Star
+          </Text>
+        </Link>
+      </Flex>
+      <Flex justify="center" alignItems="center" flex="1">
+        <Card className="mainCard" m={5} h={'80vh'} w={'90%'}>
+          <Box position="relative" mt={10} m={2} h={'100%'}>
+            <Image
+              src={parking}
+              alt="Parking"
+              borderRadius={"xl"}
+              w={"100%"}
+              h={"auto"}
+              maxH={"60vh"}
+              objectFit="cover"
+            />
+            <Text
+              textStyle={"titleSize"}
+              fontSize={theme.textStyles?.titleSize}
               position="absolute"
-              top="20%"
-              // left="50%"
-              // transform="translate(-50%, -50%)"
-              // backgroundColor={"white"}
-              // border
+              top="-0.5%"
+              left="23%"
+              transform="translate(-50%, -50%)"
+              color="black"
+              w={"100%"}
+              textAlign="center"
             >
+              Gerencie, lucre, avance
+            </Text>
+            <Box position="absolute" top="80%" left="13%" transform="translate(-50%, -50%)">
               <SimpleCard
-                showImage={true} // Altere para false para desativar a imagem
+                showImage={true}
                 imageSrc="https://via.placeholder.com/300"
                 title="Descubra o que podemos fazer juntos"
                 buttonText="Click me"
                 onButtonClick={handleButtonClick}
               />
             </Box>
-            </Box>
-          </Card>
-          <Card m={5} mb={5}>
-            <Flex>
-              <GoogleMapsCard />
-            </Flex>
-          </Card>
-        </SimpleGrid>
+          </Box>
+        </Card>
       </Flex>
     </>
   );
