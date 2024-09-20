0024fb497f6f96edb0d901a69ba984103d5d3c7f
Temistocles Zwang <temis2st@gmail.com>
2024-09-19 23:01:02 -0300

    add

--- star-frontend/vite.config.ts
+++ star-frontend/vite.config.ts
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'chart.js', 'react-chartjs-2', '@react-google-maps/api'],
-    },
-  },
-  // Define base path for proper routing in SPA
-  base: '/login'
-});
+})
